5f22c51eb1c8867d6653a7b204dd1e1cd9db292a
dbadu <dhirajbadu50@gmail.com>
2021-02-07 18:02:10 +0545

    first push

---
+++ grails-app/controllers/java_8/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package java_8
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
---
+++ grails-app/init/java_8/BootStrap.groovy
@@ -0,0 +1,9 @@
+package java_8
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
