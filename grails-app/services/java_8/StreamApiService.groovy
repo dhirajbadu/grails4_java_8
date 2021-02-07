9f1f6e5dff3dd4c2f966d746c6b8d58164185e51
dbadu <dhirajbadu50@gmail.com>
2021-02-07 18:43:41 +0545

    stream api example

---
+++ grails-app/services/java_8/StreamApiService.groovy
@@ -0,0 +1,43 @@
+package java_8
+
+import com.ClientDTO
+import com.ClientWrapper
+import grails.gorm.transactions.Transactional
+
+import java.util.stream.Collectors
+
+class StreamApiService {
+
+    def serviceMethod() {
+
+    }
+
+    private def convertToClientWrapper(ClientDTO clientDTO){
+        if (clientDTO == null){
+            return null;
+        }
+
+        return new ClientWrapper(clientDTO.id , clientDTO.firstName + " " + clientDTO.lastName)
+    }
+
+    def streamMapExample(List<ClientDTO> clientDTOList){
+        clientDTOList.stream().map{convertToClientWrapper(it)}.collect(Collectors.toList())
+    }
+
+    def streamFilterExample(List<ClientDTO> clientDTOList){
+        clientDTOList.stream().filter{it.firstName == "Dhiraj"}.collect(Collectors.toList())
+    }
+
+    def streamAnyMatchExample(List<ClientDTO> clientDTOList , String match){
+        return clientDTOList.stream().anyMatch{(it.firstName.contains(match))}
+    }
+
+    def streamAllMatchExample(List<ClientDTO> clientDTOList , String match){
+        clientDTOList.stream().allMatch{it.firstName.contains(match)}
+    }
+
+    def streamPaginationExample(List<ClientDTO> clientDTOList , int offset , int max){
+        clientDTOList.stream().skip(offset).limit(max).collect(Collectors.toList())
+    }
+
+}
---
+++ src/main/groovy/com/ClientWrapper.java
@@ -0,0 +1,28 @@
+package com;
+
+public class ClientWrapper {
+
+    private long id;
+    private String fullName;
+
+    public ClientWrapper(long id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
